823a189f7ef3e85fd3bab4855b72d847d888168d
Alex Kazumi Yoshitake <alex.yoshitake@gmail.com>
2021-01-21 00:02:26 -0300

    4.3. Atualizando pessoa com PUT

--- src/main/java/com/algaworks/algamoney/api/PessoaResource.java
+++ src/main/java/com/algaworks/algamoney/api/PessoaResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import com.algaworks.algamoney.api.event.RecursoCriadoEvent;
 import com.algaworks.algamoney.api.model.Pessoa;
 import com.algaworks.algamoney.api.repository.PessoaRepository;
+import com.algaworks.algamoney.api.service.PessoaService;
 
 @Controller
 @RequestMapping("/pessoas")
@@ -31,6 +33,9 @@ public class PessoaResource {
 
 	@Autowired
 	private ApplicationEventPublisher publisher;
+
+	@Autowired
+	private PessoaService pessoaService;
 
 	@PostMapping
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
@@ -54,5 +59,10 @@ public class PessoaResource {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deletar(@PathVariable Long codigo) {
 		pessoaRepository.deleteById(codigo);
+	}
+
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @RequestBody Pessoa pessoa) {
+		return ResponseEntity.ok(pessoaService.atualizar(codigo, pessoa));
 	}
 }
